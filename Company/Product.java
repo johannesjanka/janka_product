7a644a1e0729dd90be6ff0b4fd2110e747aa460b
Johannes Janka <Johannes Janka>
2017-03-20 13:40:28 +0100

    Company Product

---
+++ Company/Product.java
@@ -0,0 +1,18 @@
+public class Product
+{
+  private String name = "Coca Cola";
+  
+  public Product()
+  {
+  }
+  
+  public String getName()
+  {
+    return name;
+  }
+  
+  public void setName(String name)
+  {
+    this.name = name;
+  }
+}
